a39d47d4f361eb1cc50092c7085ae77aa722c51a
abhilashraut <85395848+abhilashraut@users.noreply.github.com>
2021-06-06 13:55:54 +0530

    Create product1.cpp
    will print product for the two decimal numbers .

---
+++ product1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  double x,y;
+  double product;
+  cout<<" enter first number : ";
+  cin>>x;
+  cout<<" enter second number : ";
+  cin>>y;
+  product = x*y;
+  cout<< "the product is : "<<product;
+  return 0;
+}
